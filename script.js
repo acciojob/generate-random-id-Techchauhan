a2eb7d5c5760b64538a91fcf30806b219e17bb78
Techchauhan <88832979+Techchauhan@users.noreply.github.com>
2024-12-04 21:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+
+	for(let i = 0; i<=l; i++){
+		let radomIndex = Math.floor(Math.random()*characters.length);
+		result += characters[radomIndex];
+	}
+
+	return result;
 }
 
 // Do not change the code below.
